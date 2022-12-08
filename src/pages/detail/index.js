14c690134f5835dfcd1fa825a9d979c35d168e7f
Humfrey <apple@Humfrey-MacBook-Pro.local>
2022-12-08 20:26:54 +0700

    API detail mobil calendar

--- src/pages/detail/index.js
+++ src/pages/detail/index.js
@@ -1,41 +1,82 @@
-import React, { useState, useEffect, useRef } from 'react';
-import Axios from 'axios';
-import { useParams } from 'react-router-dom';
-import './style.scss'
+import React, { useState, useEffect, useRef } from "react";
+import Axios from "axios";
+import { useParams } from "react-router-dom";
+import "./style.scss";
 import "../../assets/css/bootstrap.css";
 import Accordion from "../../../node_modules/react-bootstrap/Accordion";
-import Calendar from "react-calendar";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+// import { createRoot } from "react-dom/client";
+import {queryData} from "../../helper";
 
 const Detail = () => {
-
+  //Calendar
+  const [dateRange, setDateRange] = useState([null, null]);
+  const [startDate, endDate] = dateRange;
+  const [emptyData, setEmptyData] = useState(false);
   const [detail, setDetail] = useState({});
-  const [value, onChange] = useState(new Date());
+  // const [value, onChange] = useState(new Date());
   let { productId } = useParams();
 
   const baseUrl = "https://bootcamp-rent-cars.herokuapp.com";
 
   const fetch = useRef(true);
-  
+
   const getDetail = (productId) => {
     Axios.get(`${baseUrl}/customer/car/${productId}`)
-    .then((response) => {
-      console.log(response.data)
-      setDetail(response.data);
+      .then((response) => {
+        console.log(response.data);
+        setDetail(response.data);
       })
       .catch((error) =>
         // handle error
         console.log(error)
       );
   };
+
+  
+  const pushData = (e) => {
+    e.preventDefault();
+    const params = {
+      startRentAt: startDate.current.value,
+      finishRentAt: endDate.current.value,
+    };
+    setEmptyData(false)
+    Axios.post(`${baseUrl}/customer/order?${queryData(params)}`)
+      .then((response) => {
+        if (response.data.cars.length != null) {
+          setDateRange(response.data.startDate);
+          setDateRange(response.data.endDate);
+          console.log(response.data.startDate);
+        } else {
+          setEmptyData(true);
+        }
+        // if (response) {
+        //   setTimeout(() => {
+        //     if (response.data.length > 0) {
+        //       const descending = response.data.sort((a, b) => b.id - a.id);
+        //       setCars(descending);
+        //     } else {
+        //       setEmptyData(true);
+        //     }
+        //     setLoading(false);
+        //   }, 2000);
+        // }
+      })
+      .catch((error) => console.log(error));
+  };
+
   // console.log(carCategory);
 
   useEffect(() => {
     if (fetch.current) {
       fetch.current = false;
-      getDetail(productId)
+      getDetail(productId);
     }
   }, [productId]);
-  
+
+  //Put Function
+
   return (
     <div className="detail">
       <div className="container">
@@ -83,14 +124,27 @@ const Detail = () => {
               <img src={detail.image} alt="" />
             </div>
             <h1>{detail.name}</h1>
-            <div>
-              <Calendar onChange={onChange} value={value} />
+            <div className="datepicker">
+              <DatePicker
+                selectsRange={true}
+                placeholderText="Pilih tanggal mulai dan tanggal akhir sewa"
+                startDate={startDate}
+                endDate={endDate}
+                onChange={(update) => {
+                  setDateRange(update);
+                }}
+                isClearable={true}
+                className="datepicker"
+              />
             </div>
             <div className="total">
               <p>Total</p>
               <p>{detail.price}</p>
             </div>
-            <button className="btn btn-success">Lanjutkan Pembayaran</button>
+            <button className="btn btn-success" onClick={pushData}>
+              Lanjutkan Pembayaran
+            </button>
+            {emptyData && <>Data Tidak Ditemukan</>}
           </div>
         </div>
       </div>
--- src/pages/e_tiket/index.js
+++ src/pages/e_tiket/index.js
@@ -1,9 +1,18 @@
 import React from "react";
 import successIcon from "./success.svg";
-import download from "./fi_download.svg"
-import './style.scss'
+import download from "./fi_download.svg";
+// PDF
+import { Worker, Viewer } from "@react-pdf-viewer/core";
+import { getFilePlugin, RenderDownloadProps } from "@react-pdf-viewer/get-file";
+import Portofolio from "./test.pdf";
+import "@react-pdf-viewer/core/lib/styles/index.css";
 
-const E_Tiket = () => {
+import "./style.scss";
+
+const E_Tiket = (props) => {
+  // PDF Viewer
+  const getFilePluginInstance = getFilePlugin();
+  const { Download } = getFilePluginInstance;
   return (
     <div className="e_tiket">
       <div className="container">
@@ -14,23 +23,44 @@ const E_Tiket = () => {
             <p>Tunjukkan invoice ini ke petugas BCR di titik temu.</p>
           </div>
         </div>
-				<div className="row">
-					<div className="col-12">
-						<div className="card">
-							<div className="row">
-								<div className="col-8">
-									<h2>Invoice</h2>
-								</div>
-								<div className="col-4">
-									<button className="button"> 
-										<img src={download} alt="" />
-										unduh</button>
-								</div>
-							</div>
-							<p>no. inovice</p>
-						</div>
-					</div>
-				</div>
+        <div className="row invoice-detail">
+          <div className="col-12">
+            <div className="card">
+              <div className="row">
+                <div className="col-8">
+                  <h2>Invoice</h2>
+                </div>
+                <div className="col-4">
+                  {/* <Download>
+                    {(RenderDownloadProps) => (
+                      <button className="button" onClick={props.onClick}>
+                        <img src={download} alt="" />
+                        Unduh
+                      </button>
+                    )}
+                  </Download> */}
+                </div>
+              </div>
+              <p>no. inovice</p>
+              <div className="row invoice-view">
+                <div className="col-12">
+                <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.0.279/build/pdf.worker.min.js">
+                  <div
+                    style={{
+                      height: "500px"
+                    }}
+                  >
+                    <Viewer
+                      fileUrl={Portofolio}
+                      plugins={[getFilePluginInstance]}
+                    />
+                  </div>
+                </Worker>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
